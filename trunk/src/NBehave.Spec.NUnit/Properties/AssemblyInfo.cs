34c948d41d12748b8ff9136fc17a794f97c551c9
lazydev <lazydev@76abf7ef-5a29-0410-9a71-e7582cf7e72e>
2008-11-06 09:15:41 +0000

    use CommonAssemblyInfo.cs
    git-svn-id: http://nbehave.googlecode.com/svn@66 76abf7ef-5a29-0410-9a71-e7582cf7e72e

--- trunk/src/NBehave.Spec.NUnit/Properties/AssemblyInfo.cs
+++ trunk/src/NBehave.Spec.NUnit/Properties/AssemblyInfo.cs
@@ -1,36 +1,5 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("NBehave.Spec.NUnit")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Dell, Inc")]
-[assembly: AssemblyProduct("NBehave.Spec.NUnit")]
-[assembly: AssemblyCopyright("Copyright © Dell, Inc 2008")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c6e9073e-1abc-45c8-8ae6-1cdbbcd41ba3")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
