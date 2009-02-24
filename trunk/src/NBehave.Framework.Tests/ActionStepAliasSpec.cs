d501571672e28ad08a55cdc151def1f33402c601
lazydev <lazydev@76abf7ef-5a29-0410-9a71-e7582cf7e72e>
2009-02-24 21:02:21 +0000

    added alias And for Given/Then in text scenarios. Can be used for localized Given/When/Then also (ex swedish). Files I forgot to add
    git-svn-id: http://nbehave.googlecode.com/svn@89 76abf7ef-5a29-0410-9a71-e7582cf7e72e

---
+++ trunk/src/NBehave.Framework.Tests/ActionStepAliasSpec.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NBehave.Narrator.Framework;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+using Rhino.Mocks;
+using Context = NUnit.Framework.TestFixtureAttribute;
+using Specification = NUnit.Framework.TestAttribute;
+
+namespace NBehave.Narrator.Framework.Specifications
+{
+    public class ErgoAttribute : ActionStepAttribute
+    {
+        //Ergo = Given in latin, from http://www.freedict.com/onldict/onldict.php
+        public ErgoAttribute(string tokenString)
+            : base("Ergo " + tokenString)
+        { }
+    }
+
+    [Context]
+    public class When_registering_alias_for_ActionStepAttribute
+    {
+        [Specification]
+        public void ShouldRegisterAndAsDefaultAliasForGiven()
+        {
+            var actionStepAlias = new ActionStepAlias();
+            var aliases = actionStepAlias.GetAliasFor(typeof(GivenAttribute));
+            Assert.That(aliases.Count(), Is.EqualTo(1));
+            Assert.That(aliases.First(), Is.EqualTo("And"));
+        }
+
+        [Specification]
+        public void ShouldRegisterAndAsDefaultAliasForThen()
+        {
+            var actionStepAlias = new ActionStepAlias();
+            var aliases = actionStepAlias.GetAliasFor(typeof(ThenAttribute));
+            Assert.That(aliases.Count(), Is.EqualTo(1));
+            Assert.That(aliases.First(), Is.EqualTo("And"));
+        }
+
+        [Specification]
+        public void ShouldRegisterAndAsDefaultAliasForErgo()
+        {
+            var actionStepAlias = new ActionStepAlias();
+            IEnumerable<string> aliases = actionStepAlias.GetAliasFor(typeof(ErgoAttribute));
+            Assert.That(aliases.Count(), Is.EqualTo(3));
+            Assert.That(aliases.First(), Is.EqualTo("Tunc"));
+            Assert.That(aliases.Skip(1).First(), Is.EqualTo("Deinde"));
+            Assert.That(aliases.Skip(2).First(), Is.EqualTo("Mox"));
+
+        }
+
+        [Specification]
+        public void ShouldGetAliasForTokenString()
+        {
+            string tokenStringTemplate = "{0} err, I dont know latin!";
+            string tokenString = string.Format(tokenStringTemplate, "Ergo");
+            var actionStepAlias = new ActionStepAlias();
+            IEnumerable<string> aliases = actionStepAlias.GetAliasForTokenString(tokenString);
+            Assert.That(aliases.Count(), Is.EqualTo(3));
+            Assert.That(aliases.First(), Is.EqualTo(string.Format(tokenStringTemplate, "Tunc")));
+            Assert.That(aliases.Skip(1).First(), Is.EqualTo(string.Format(tokenStringTemplate, "Deinde")));
+            Assert.That(aliases.Skip(2).First(), Is.EqualTo(string.Format(tokenStringTemplate, "Mox")));
+        }
+
+        [Specification]
+        public void ShouldGetDefaultAliasForGiven()
+        {
+            string tokenString = "Given something";
+            var actionStepAlias = new ActionStepAlias();
+            IEnumerable<string> aliases = actionStepAlias.GetAliasForTokenString(tokenString);
+            Assert.That(aliases.Count(), Is.EqualTo(1));
+            Assert.That(aliases.First(), Is.EqualTo("And something"));
+        }
+    }
+}
---
+++ trunk/src/NBehave.Framework/ActionStepAlias.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+
+namespace NBehave.Narrator.Framework
+{
+    public static class ActionStepAliasConfiguration
+    {
+        private static NameValueCollection _config = ConfigurationManager.GetSection("NBehave") as NameValueCollection;
+
+        public static IEnumerable<string> GetAliasesForAttribute(Type actionStepAttribute)
+        {
+            return GetAliasesForAttribute(actionStepAttribute.Name.Replace("Attribute", ""));
+        }
+
+        public static IEnumerable<string> GetAliasesForAttribute(string actionStep)
+        {
+            string value = _config[string.Format("Alias.{0}", actionStep)] ?? string.Empty;
+            return value.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+
+    public class ActionStepAlias
+    {
+        public IEnumerable<string> GetAliasFor(Type actionStepsAttribute)
+        {
+            IEnumerable<string> aliasForAttribute = ActionStepAliasConfiguration.GetAliasesForAttribute(actionStepsAttribute);
+
+            return AddDefaultAlias(aliasForAttribute, TypeAsStringWithoutAttributeAtEnd(actionStepsAttribute));
+        }
+
+        private IEnumerable<string> AddDefaultAlias(IEnumerable<string> aliasList, string magicWord)
+        {
+            var defaultAliasForGiven = new List<string> { "And" };
+            var defaultAliasForThen = new List<string> { "And" };
+
+
+            if ((magicWord == TypeAsStringWithoutAttributeAtEnd(typeof(GivenAttribute)))
+                || (magicWord == TypeAsStringWithoutAttributeAtEnd(typeof(ThenAttribute))))
+                return JoinLists(defaultAliasForGiven, aliasList);
+            return aliasList;
+        }
+
+        private string TypeAsStringWithoutAttributeAtEnd(Type type)
+        {
+            return type.Name.Replace("Attribute", "");
+        }
+
+        private static IEnumerable<string> JoinLists(IEnumerable<string> defaultAliasForGiven, IEnumerable<string> aliasForAttribute)
+        {
+            List<string> list = new List<string>(aliasForAttribute);
+            list.AddRange(defaultAliasForGiven);
+            return list;
+        }
+
+        public IEnumerable<string> GetAliasForTokenString(string tokenString)
+        {
+            int startIndex = tokenString.IndexOf(' ') != -1 ? tokenString.IndexOf(' ') : tokenString.Length;
+            string firstWord = tokenString.Substring(0, startIndex);
+            IEnumerable<string> aliasForAttribute = AddDefaultAlias(ActionStepAliasConfiguration.GetAliasesForAttribute(firstWord), firstWord);
+            var tokenAliases = new List<string>();
+            string restOfToken = tokenString.Substring(startIndex);
+            foreach (var tokenAlias in aliasForAttribute)
+            {
+                tokenAliases.Add(tokenAlias + restOfToken);
+            }
+            return tokenAliases;
+        }
+    }
+}
