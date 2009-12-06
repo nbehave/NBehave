8e546b5e4cf323e2c4ad24abc5b1cfe13101086e
Lazydev <Lazydev@76abf7ef-5a29-0410-9a71-e7582cf7e72e>
2009-12-06 21:05:23 +0000

    Game of life example
    git-svn-id: http://nbehave.googlecode.com/svn@184 76abf7ef-5a29-0410-9a71-e7582cf7e72e

---
+++ trunk/src/NBehave.Examples/Game of life/Specs/GameSpecs.cs
@@ -0,0 +1,31 @@
+﻿using NBehave.Narrator.Framework;
+using NBehave.Spec.NUnit;
+
+namespace NBehave.Examples.GameOfLife.Specs
+{
+    [ActionSteps]
+    public class GameSpecs
+    {
+        private Game _game;
+
+        [Given(@"a (?<width>\d+) by (?<height>\d+) game")]
+        [Given(@"a new game: (?<width>\d+) by (?<height>\d+)")]
+        public void GameOfLife(int width, int height)
+        {
+            _game = new Game(width, height);
+        }
+
+        [When(@"I toggle the cell at \((?<x>\d+), (?<y>\d+)\)")]
+        public void ToggleCell(int x, int y)
+        {
+            _game.ToggleCell(x, y);
+        }
+
+        [Then(@"the grid should look like\s+(?<rows>(.+\s*)+)")]
+        [Then(@"the grid should be\s+(?<rows>(.+\s*)+)")]
+        public void GameLooksLike(string rows)
+        {
+            _game.ToString().ShouldEqual(rows);
+        }
+    }
+}
