1a8dd032c335e6de52e101266f099b929bf9954d
anyerr <anyerr@76abf7ef-5a29-0410-9a71-e7582cf7e72e>
2010-04-27 23:11:15 +0000

    Issue 33 implemented as far as possible - TestDriven.Net integration. Patch from Alan Gardner. Unfortunately it is not possible (currently) to run an individual scenario file due to limitations in TestDriven.Net.
    git-svn-id: http://nbehave.googlecode.com/svn@211 76abf7ef-5a29-0410-9a71-e7582cf7e72e

--- trunk/src/NBehave.TestDriven.Plugin/NBehaveStoryRunner.cs
+++ trunk/src/NBehave.TestDriven.Plugin/NBehaveStoryRunner.cs
@@ -1,4 +1,7 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 
 using NBehave.Narrator.Framework;
 using TestDriven.Framework;
@@ -8,43 +11,49 @@ namespace NBehave.TestDriven.Plugin
 {
     public class NBehaveStoryRunner : ITestRunner
     {
+        private TextRunner _runner;
+
         TestRunState ITestRunner.RunAssembly(ITestListener tddNetListener, Assembly assembly)
         {
-            return Run(assembly, null, tddNetListener);
+            return Run(assembly, null, tddNetListener);            
         }
 
         TestRunState ITestRunner.RunMember(ITestListener tddNetListener, Assembly assembly, MemberInfo member)
         {
-            return Run(assembly, member, tddNetListener);
+            return Run(assembly, member, tddNetListener);            
         }
-      
+
         TestRunState ITestRunner.RunNamespace(ITestListener testListener, Assembly assembly, string ns)
         {
             //TODO: Fix filter. ns is probably equal to NamespaceFilter in StoryRunnerFilter
-            return Run(assembly, null, testListener);
+            return Run(assembly, null, testListener);      
         }
-
 
         private TestRunState Run(Assembly assembly, MemberInfo member, ITestListener tddNetListener)
         {
             var listener = new StoryRunnerEventListenerProxy(tddNetListener);
-            FeatureResults results = RunStories(assembly, member, listener);
+            _runner = new TextRunner(listener);
+            
+            var locator = new StoryLocator
+                            {
+                                RootLocation = Path.GetDirectoryName(assembly.Location)
+                            };
+
+            var type = member as Type;
+            IEnumerable<string> stories = (type == null)
+                                      ? locator.LocateAllStories() 
+                                      : locator.LocateStoriesMatching(type);
+            
+            _runner.Load(stories);
+            _runner.StoryRunnerFilter = StoryRunnerFilter.GetFilter(member);
+            _runner.LoadAssembly(assembly);
+
+            FeatureResults results = _runner.Run();
 
             return GetTestRunState(results);
         }
 
-        private FeatureResults RunStories(Assembly assembly, MemberInfo member, IEventListener listener)
-        {
-            RunnerBase runner = new TextRunner(listener);
-            runner.StoryRunnerFilter = StoryRunnerFilter.GetFilter(member);
-            runner.LoadAssembly(assembly);
-
-            FeatureResults results = runner.Run();
-
-            return results;
-        }
-
-        private TestRunState GetTestRunState(FeatureResults results)
+        private static TestRunState GetTestRunState(FeatureResults results)
         {
             TestRunState state = TestRunState.Success;
             if (results.NumberOfFailingScenarios > 0)
--- trunk/src/NBehave.TestDriven.Plugin/StoryRunnerEventListenerProxy.cs
+++ trunk/src/NBehave.TestDriven.Plugin/StoryRunnerEventListenerProxy.cs
@@ -6,10 +6,12 @@ using TestDriven.Framework;
 
 namespace NBehave.TestDriven.Plugin
 {
+    /// <summary>
+    /// This class assumed to be tied to TDD.Net implementation
+    /// </summary>
     public class StoryRunnerEventListenerProxy : IEventListener
     {
         private readonly ITestListener _listener;
-        private readonly List<ScenarioResult> _allResults = new List<ScenarioResult>();
 
         public StoryRunnerEventListenerProxy(ITestListener listener)
         {
@@ -43,52 +45,21 @@ namespace NBehave.TestDriven.Plugin
         }
 
         void IEventListener.ScenarioResult(ScenarioResult result)
-        {
-            _allResults.Add(result);
+        {                        
             _listener.WriteLine(string.Format("\t\tScenario: {0} - {1}", result.ScenarioTitle, result.Result), Category.Info);
+            _listener.TestFinished(
+                new TestResult
+                {
+                    Name = result.ScenarioTitle,
+                    Message = result.Result.Message,
+                    State = result.Result.ToTestState(),
+                    StackTrace = result.StackTrace
+                }); 
         }
 
         void IEventListener.RunFinished()
         {
-            var summary = new TestResult();
-            summary.TotalTests = _allResults.Count;
-            summary.State = TestState.Passed;
-            summary.Message = CreateSummary();
-            _listener.TestFinished(summary);
-        }
-
-        private string CreateSummary()
-        {
-            var features = _allResults.Select(f => f.FeatureTitle).Distinct().Count();
-            var allScenarios = _allResults.Count();
-            var pendingScenarios = _allResults.Select(s => s.Result is Pending).Count();
-            var failedScenarios = _allResults.Select(s => s.Result is Failed).Count();
-            var allSteps = GetAllSteps();
-            var pendingSteps = allSteps.Select(f => f.Result is Pending).Count();
-            var failedSteps = allSteps.Select(f => f.Result is Failed).Count();
-            string summary = string.Format("Features: {1}{0}Scenarios: {2}, scenarios pending: {3}, scenarios failed: {4}{0}Steps: {5}, steps pending: {6}, steps failed: {7}",
-                                           Environment.NewLine, features, allScenarios, pendingScenarios, failedScenarios, allSteps.Count, pendingSteps, failedSteps);
-            return summary;
-        }
-
-        private List<ActionStepResult> GetAllSteps()
-        {
-            var steps = new List<ActionStepResult>();
-
-            foreach (var result in _allResults)
-                steps.AddRange(result.ActionStepResults);
-            return steps;
-        }
-
-        private TestState GetTestResultState(Result result)
-        {
-            if (result.GetType() == typeof(Passed))
-                return TestState.Passed;
-            if (result.GetType() == typeof(Failed))
-                return TestState.Failed;
-            if (result.GetType() == typeof(Pending))
-                return TestState.Ignored;
-            throw new NotSupportedException(string.Format("Result {0} isn't supported.", result));
+            //results now handled one at a time        
         }
 
     }
