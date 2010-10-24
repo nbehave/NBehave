06714b57f3b96347276ed7e1c9ec06b37ace8382
naeem.khedarun <devnull@localhost>
2010-10-24 17:06:47 +0100

    Updated version

--- src/NBehave.VS2010.Plugin/Editor/Resources/Logo.xaml.cs
+++ src/NBehave.VS2010.Plugin/Editor/Resources/Logo.xaml.cs
@@ -1,24 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
-namespace NBehave.VS2010.Plugin
+﻿namespace NBehave.VS2010.Plugin
 {
-	public partial class Logo
-	{
-		public Logo()
-		{
-			this.InitializeComponent();
+    public partial class Logo
+    {
+        public Logo()
+        {
+            this.InitializeComponent();
 
-			// Insert code required on object creation below this point.
-		}
-	}
+            // Insert code required on object creation below this point.
+        }
+    }
 }
