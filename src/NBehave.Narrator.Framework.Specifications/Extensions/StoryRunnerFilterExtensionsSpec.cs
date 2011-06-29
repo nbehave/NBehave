f3dc8e6a99c1ae2ec53a8f4d276c2db5c3a0a4f2
Morgan Persson <morgan.persson@factor10.com>
2011-06-29 16:08:08 +0200

    Added another feature from palazkov's patch. extension methods on StoryRunnerFilter

---
+++ src/NBehave.Narrator.Framework.Specifications/Extensions/StoryRunnerFilterExtensionsSpec.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using NUnit.Framework;
+
+namespace NBehave.Narrator.Framework.Specifications.Extensions
+{
+    [TestFixture]
+    public class StoryRunnerFilterExtensionsSpec
+    {
+        [Test]
+        public void Should_build_pattern_based_on_class_name()
+        {
+            var filter = In.Context<StoryRunnerFilterExtensionsSpec>();
+            Assert.AreEqual("^StoryRunnerFilterExtensionsSpec$", filter.ClassNameFilter.ToString());
+        }
+
+        [Test]
+        public void Should_build_pattern_based_on_class_name_using_global_context()
+        {
+            var filter = In.GlobalContext().And<StoryRunnerFilterExtensionsSpec>();
+            Assert.AreEqual("^StoryRunnerFilterExtensionsSpec$", filter.ClassNameFilter.ToString());
+        }
+
+        [Test]
+        public void Should_build_pattern_based_on_multiple_class_names()
+        {
+            var filter = In.Context<StoryRunnerFilterExtensionsSpec>().And<StringBuilder>().And<TestAttribute>();
+            Assert.AreEqual("^StoryRunnerFilterExtensionsSpec|StringBuilder|TestAttribute$", filter.ClassNameFilter.ToString());
+        }
+    }
+}
