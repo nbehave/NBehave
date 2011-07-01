f94d5dccefaaf572cd52fa713ba69cb113832245
Morgan Persson <morgan.persson@factor10.com>
2011-07-01 22:25:05 +0200

    Removed unused code

--- src/NBehave.ReSharper.Plugin/UnitTestProvider/MetadataExplorer.cs
+++ src/NBehave.ReSharper.Plugin/UnitTestProvider/MetadataExplorer.cs
@@ -3,18 +3,13 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using JetBrains.ProjectModel;
-using JetBrains.ReSharper.Psi;
-using JetBrains.ReSharper.Psi.Util;
 using JetBrains.ReSharper.UnitTestFramework;
-using JetBrains.ReSharper.UnitTestFramework.AttributeChecker;
 using NBehave.Narrator.Framework;
 
 namespace NBehave.ReSharper.Plugin.UnitTestProvider
 {
     public class MetadataExplorer
     {
-        private static readonly IClrTypeName ActionStepsAttribute = new ClrTypeName(typeof(ActionStepsAttribute).FullName);
-        private static readonly IClrTypeName ActionStepAttribute = new ClrTypeName(typeof(ActionStepAttribute).FullName);
         private readonly TestProvider _testProvider;
         private readonly UnitTestElementConsumer _consumer;
         private readonly IProject _project;
@@ -51,47 +46,6 @@ namespace NBehave.ReSharper.Plugin.UnitTestProvider
         {
             foreach (var feature in features)
                 _consumer(feature);
-        }
-
-        public static bool IsActionStepMethod(ITypeMember element, UnitTestAttributeCache attributeChecker)
-        {
-            var method = element as IMethod;
-            return method != null
-                   && !method.IsAbstract
-                   && method.GetAccessRights() == AccessRights.PUBLIC
-                   && !method.TypeParameters.Any()
-                   && HasAnyAttributeOrDerivedAttribute(method, attributeChecker, new[] { ActionStepsAttribute, ActionStepAttribute });
-        }
-
-        public static bool IsActionStepsClass(ITypeElement typeElement, out bool isAbstract, UnitTestAttributeCache attributeChecker)
-        {
-            isAbstract = false;
-            var @class = typeElement as IClass;
-            if (@class == null)
-                return false;
-            var modifiersOwner = (IModifiersOwner)typeElement;
-            isAbstract = modifiersOwner.IsAbstract;
-            AccessRights accessRights = modifiersOwner.GetAccessRights();
-            if (accessRights != AccessRights.PUBLIC)
-                return false;
-            if (HasAttributeOrDerivedAttribute(typeElement, ActionStepsAttribute, attributeChecker))
-                return true;
-            return (
-                       @class
-                       .GetAllSuperClasses()
-                       .Select(superType => superType.GetTypeElement()))
-                       .Any(superClass => superClass != null && HasAttributeOrDerivedAttribute(superClass, ActionStepsAttribute, attributeChecker))
-                   || typeElement.GetMembers().Any(member => IsActionStepMethod(member, attributeChecker));
-        }
-
-        private static bool HasAttributeOrDerivedAttribute(IAttributesOwner member, IClrTypeName attribute, UnitTestAttributeCache attributeChecker)
-        {
-            return attributeChecker.HasAttributeOrDerivedAttribute(member, attribute);
-        }
-
-        private static bool HasAnyAttributeOrDerivedAttribute(IAttributesOwner member, UnitTestAttributeCache attributeChecker, params IClrTypeName[] attributes)
-        {
-            return attributeChecker.HasAttributeOrDerivedAttribute(member, attributes);
         }
     }
 }
