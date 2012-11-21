c18f5be3d695df70dcd21347336092e9e9f8859e
Morgan Persson <lazydev@gmail.com>
2012-11-21 22:27:07 +0100

    Fixed id of some R# nuget packages & renamed R# project files

--- nuget.ps1
+++ nuget.ps1
@@ -16,7 +16,7 @@ task NuGet -depends NugetClean -precondition{ return $frameworkVersion -eq "4.0"
 	Exec { .\src\.nuget\nuget.exe pack nuget\nbehave.runners.nuspec -Version $buildNumber -OutputDirectory $artifactsDir}
 	Exec { .\src\.nuget\nuget.exe pack nuget\nbehave.samples.nuspec -Version $buildNumber -OutputDirectory $artifactsDir}
 	Exec { .\src\.nuget\nuget.exe pack nuget\NBehave.Resharper60.nuspec -Version $buildNumber -OutputDirectory $artifactsDir}
-	Exec { .\src\.nuget\nuget.exe pack nuget\NBehave.Resharper61.nuspec -Version $buildNumber -OutputDirectory $artifactsDir}
+	Exec { .\src\.nuget\nuget.exe pack nuget\NBehave.Resharper611.nuspec -Version $buildNumber -OutputDirectory $artifactsDir}
 	Exec { .\src\.nuget\nuget.exe pack nuget\NBehave.Resharper701.nuspec -Version $buildNumber -OutputDirectory $artifactsDir}
 	Exec { .\src\.nuget\nuget.exe pack nuget\NBehave.Resharper71.nuspec -Version $buildNumber -OutputDirectory $artifactsDir}
 	#Exec { .\src\.nuget\nuget.exe pack nuget\NBehave.VsPlugin.nuspec -Version $buildNumber -OutputDirectory $artifactsDir}
