ed899f002b6afb6f27eaa039a1ae5ba7ea979961
Morgan Persson <lazydev@gmail.com>
2012-12-30 00:32:36 +0100

    set version to 0.6.2

--- BuildProperties.ps1
+++ BuildProperties.ps1
@@ -1,5 +1,5 @@
 properties {
-	$version            = "0.6.1"
+	$version            = "0.6.2"
 	$assemblyVersion    = AssemblyVersion
 	$assemblyInfoVersion= AssemblyInformationalVersion
 	$nugetVersionNumber = AssemblyInformationalVersion
