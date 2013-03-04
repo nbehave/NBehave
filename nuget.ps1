183c97e6f05714711fd7726fde1603de95fdd3d2
Morgan Persson <lazydev@gmail.com>
2013-03-04 23:05:04 +0100

    Fixed bug with R# nuget packages and added packages for R# 7.1.1 and R# 7.1.2

--- nuget.ps1
+++ nuget.ps1
@@ -11,16 +11,29 @@ task NugetClean {
 	Remove-Item $artifactsDir\*.nuget
 }
 
+function resharper_install_scripts($version) {
+	$content = get-content ".\nuget\Resharper.scripts\Install.ps1"
+	$out = $content -replace "%version%", "$version"
+	set-content -Path ".\build\Install.ps1" -Value $out
+	$content = get-content ".\nuget\Resharper.scripts\UnInstall.ps1"
+	$out = $content -replace "%version%", "$version"
+	set-content -Path ".\build\UnInstall.ps1" -Value $out
+}
+
 task NuGet -depends NugetClean -precondition{ return $frameworkVersion -eq "4.0" } {
 	Exec { .\src\.nuget\nuget.exe pack nuget\nbehave.nuspec -Version $nugetVersionNumber -OutputDirectory $artifactsDir}
 	Exec { .\src\.nuget\nuget.exe pack nuget\nbehave.runners.nuspec -Version $nugetVersionNumber -OutputDirectory $artifactsDir}
 	Exec { .\src\.nuget\nuget.exe pack nuget\nbehave.samples.nuspec -Version $nugetVersionNumber -OutputDirectory $artifactsDir}
+	resharper_install_scripts "6.0"
 	Exec { .\src\.nuget\nuget.exe pack nuget\NBehave.Resharper60.nuspec -Version $nugetVersionNumber -OutputDirectory $artifactsDir}
+	resharper_install_scripts "6.1"
 	Exec { .\src\.nuget\nuget.exe pack nuget\NBehave.Resharper611.nuspec -Version $nugetVersionNumber -OutputDirectory $artifactsDir}
+	resharper_install_scripts "7.0"
 	Exec { .\src\.nuget\nuget.exe pack nuget\NBehave.Resharper701.nuspec -Version $nugetVersionNumber -OutputDirectory $artifactsDir}
+	resharper_install_scripts "7.1"
 	Exec { .\src\.nuget\nuget.exe pack nuget\NBehave.Resharper71.nuspec -Version $nugetVersionNumber -OutputDirectory $artifactsDir}
 	Exec { .\src\.nuget\nuget.exe pack nuget\NBehave.Resharper711.nuspec -Version $nugetVersionNumber -OutputDirectory $artifactsDir}
-	#Exec { .\src\.nuget\nuget.exe pack nuget\NBehave.VsPlugin.nuspec -Version $nugetVersionNumber -OutputDirectory $artifactsDir}
+	Exec { .\src\.nuget\nuget.exe pack nuget\NBehave.Resharper712.nuspec -Version $nugetVersionNumber -OutputDirectory $artifactsDir}
 }
 
 task NuGet-Fluent -depends NugetClean -precondition{ return $frameworkVersion -eq "4.0" } {
---
+++ src/NBehave.Resharper712.Plugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using JetBrains.Application.PluginSupport;
+
+[assembly: AssemblyTitle("NBehave.Resharper.Plugin")]
+[assembly: Guid("27171D3A-23F8-485B-A73C-8248F73F559B")]
+
+[assembly: PluginTitle("NBehave R# 7.1.2 runner")]
+[assembly: PluginDescription("Run text based feature files with NBehave")]
+[assembly: PluginVendor("NBehave")]
