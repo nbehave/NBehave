1c21502ac2e298a24904fd180fa9394bfd58e018
Morgan Persson <lazydev@gmail.com>
2013-08-01 16:46:48 +0200

    dont update fake on every build

--- build.cmd
+++ build.cmd
@@ -4,6 +4,8 @@
 @REM ********
 @ECHO OFF
 CLS
-@REM .\src\.nuget\NuGet.exe install FAKE -OutputDirectory .\buildframework\ -ExcludeVersion
+IF EXIST .\buildframework\FAKE GOTO RunBuild
+.\src\.nuget\NuGet.exe install FAKE -OutputDirectory .\buildframework\ -ExcludeVersion -Pre
 
+:RunBuild
 .\buildframework\FAKE\tools\FAKE.exe build.fsx %1 %2 %3 %4 %5 %6 %7 %8 %9
