a4f790aaad1e91e8a10250c58abde71833e19a76
Morgan Persson <lazydev@gmail.com>
2013-08-02 09:59:14 +0200

    gitignore made me miss some files

---
+++ src/NBehave.ReSharper.Plugin/UnitTestProvider/UnitTestMetadataExplorer8.0.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+using JetBrains.Application;
+using JetBrains.Metadata.Reader.API;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.UnitTestFramework;
+
+namespace NBehave.ReSharper.Plugin.UnitTestProvider
+{
+    public partial class UnitTestMetadataExplorer
+    {
+        public void ExploreAssembly(IProject project, IMetadataAssembly assembly, UnitTestElementConsumer consumer, ManualResetEvent exitEvent)
+        {
+            ReadLockCookie.Execute(() => new MetadataExplorer(_provider, _solution, project, consumer).ExploreProject());            
+        }
+    }
+}
