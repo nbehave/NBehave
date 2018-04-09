f87a45eeed9a088d7ecbd55d3c83fc49b24c531c
Morgan Persson <lazydev@gmail.com>
2018-04-09 08:43:49 +0200

    Updated for .net 4.5

--- buildProperties.fsx
+++ buildProperties.fsx
@@ -10,7 +10,7 @@ module Properties
   // params from teamcity
   let buildNumber       = getBuildParamOrDefault "buildNumber" "0"
   let buildTag          = getBuildParamOrDefault "buildTag" "devlocal" // For release, set this to "release"
-  let frameworkVersions = ["3.5"; "4.0"]
+  let frameworkVersions = ["4.5"]
 
   let version             = "0.7.0"
   let assemblyVersion     = version + "." + buildNumber
