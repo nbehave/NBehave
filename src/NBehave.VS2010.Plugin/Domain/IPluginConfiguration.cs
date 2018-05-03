af924578132767fdef1768eab82642c0e9c4fd4d
Morgan Persson <lazydev@gmail.com>
2018-05-03 08:06:12 +0200

    Removed R#, VS plugin and TestDriven plugins

--- src/NBehave.VS2010.Plugin/Domain/IPluginConfiguration.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel;
-
-namespace NBehave.VS2010.Plugin.Domain
-{
-    public interface IPluginConfiguration : INotifyPropertyChanged
-    {
-        bool CreateHtmlReport { get; set; }
-    }
-}
--- src/NBehave.VS2010.Plugin/Editor/Domain/GherkinText.cs
+++
@@ -1,9 +0,0 @@
-namespace NBehave.VS2010.Plugin.Editor.Domain
-{
-    public interface IGherkinText
-    {
-        string Title { get; }
-        string AsString { get; }
-        int SourceLine { get; }
-    }
-}
